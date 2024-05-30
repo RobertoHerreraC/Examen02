c1f81c6938289a6682431c203add4a0bdb94c09e
RobertoHerreraC <37085886+RobertoHerreraC@users.noreply.github.com>
2024-05-29 19:59:21 -0500

    Modificado en Github

--- Examen02/Clases/GestorPedidos.cs
+++ Examen02/Clases/GestorPedidos.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Modificado en Github
 namespace Examen02.Clases
 {
     public class GestorPedidos
